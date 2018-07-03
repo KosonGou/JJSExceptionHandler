3b02d3028c79b44ea9a5d7b85e60ce1bcf12427d
KosonGou <kosonguo@gmail.com>
2018-07-03 10:40:33 +0800

    main file added.

---
+++ main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  JJSExceptionHandler
+//
+//  Created by KosonGou on 2017/6/16.
+//  Copyright © 2017年 JJSHome. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
